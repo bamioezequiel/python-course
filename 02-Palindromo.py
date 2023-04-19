bdb3dd17ed3579bf7895f7ccc085e6122e4d5db2
ezequiel <ezequielbamiok@gmail.com>
2023-04-19 10:54:18 -0300

    palindromo

---
+++ 02-Palindromo.py
@@ -0,0 +1,20 @@
+def reverse(text):
+    reverse_text = ""
+    for letter in text:
+        reverse_text = letter + reverse_text
+    return reverse_text
+
+def is_palindromo(text):
+    formatted_text = text.lower().replace(" ", "")
+    #Option 1
+    #reverse_text = formatted_text[::-1]
+    #Option 2
+    reverse_text = reverse(formatted_text)
+
+    return formatted_text == reverse_text
+
+
+print("Abba", is_palindromo("Abba")) #True
+print("Reconocer", is_palindromo("Reconocer")) #True
+print("Amo la paloma", is_palindromo("Amo la paloma")) #True
+print("Hola Mundo", is_palindromo("Hola Mundo")) #False
